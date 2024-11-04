3f86428c120a38745bdedf1f5d32b9108184deb3
曾程 <695488997@qq.com>
2024-11-04 14:53:23 +0800

    fix:扩展short对比方法

--- tools/src/main/java/com/base/extensions/java/lang/Short/ShortOperatorExtends.java
+++ tools/src/main/java/com/base/extensions/java/lang/Short/ShortOperatorExtends.java
@@ -20,8 +20,27 @@ public abstract class ShortOperatorExtends implements ComparableUsing<Short> {
 	 * @param op  逻辑运算符
 	 * @return 逻辑运算结果
 	 */
-	public static boolean compareToUsing(@This Short the, Integer com, Operator op) {
-		var value = com.shortValue();
+	public static boolean compareToUsing(@This Short the, short com, Operator op) {
+		return switch (op) {
+			case LT -> the.compareTo(com) < 0;
+			case LE -> the.compareTo(com) <= 0;
+			case GT -> the.compareTo(com) > 0;
+			case GE -> the.compareTo(com) >= 0;
+			case EQ -> the.compareTo(com) == 0;
+			case NE -> the.compareTo(com) != 0;
+		};
+	}
+
+	/**
+	 * 逻辑运算符重载
+	 *
+	 * @param the Short 对象
+	 * @param com Integer 对象
+	 * @param op  逻辑运算符
+	 * @return 逻辑运算结果
+	 */
+	public static boolean compareToUsing(@This Short the, int com, Operator op) {
+		var value = Integer.valueOf(com).shortValue();
 		return switch (op) {
 			case LT -> the.compareTo(value) < 0;
 			case LE -> the.compareTo(value) <= 0;
@@ -40,8 +59,8 @@ public abstract class ShortOperatorExtends implements ComparableUsing<Short> {
 	 * @param op  逻辑运算符
 	 * @return 逻辑运算结果
 	 */
-	public static boolean compareToUsing(@This Short the, Byte com, Operator op) {
-		var value = com.shortValue();
+	public static boolean compareToUsing(@This Short the, byte com, Operator op) {
+		var value = Integer.valueOf(com).shortValue();
 		return switch (op) {
 			case LT -> the.compareTo(value) < 0;
 			case LE -> the.compareTo(value) <= 0;
