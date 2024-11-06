19d469de466265f7015ba4d144091d563f19a389
曾程 <695488997@qq.com>
2024-11-06 11:49:47 +0800

    fix:修改环形队列

--- tools/src/main/java/com/base/tools/queue/ZCircularQueue.java
+++ tools/src/main/java/com/base/tools/queue/ZCircularQueue.java
@@ -34,6 +34,11 @@ public class ZCircularQueue<T> {
 	private int _capacity = 1_000;
 
 	/**
+	 * 执行间隔（毫秒）
+	 */
+	private int _duration = 200;
+
+	/**
 	 * 回调方法 执行事件
 	 */
 	private final Consumer<Collection<T>> _action;
@@ -60,6 +65,17 @@ public class ZCircularQueue<T> {
 	/**
 	 * 构造函数
 	 *
+	 * @param action   定时执行的任务
+	 * @param take     每次取出元素个数
+	 * @param duration 执行间隔（毫秒）
+	 */
+	public ZCircularQueue(Consumer<Collection<T>> action, int take, int duration) {
+		this(0, action, take, duration);
+	}
+
+	/**
+	 * 构造函数
+	 *
 	 * @param capacity 队列容量
 	 * @param action   定时执行的任务
 	 */
@@ -75,14 +91,29 @@ public class ZCircularQueue<T> {
 	 * @param take     每次取出元素个数
 	 */
 	public ZCircularQueue(int capacity, Consumer<Collection<T>> action, int take) {
+		this(capacity, action, take, 0);
+	}
+
+	/**
+	 * 构造函数
+	 *
+	 * @param capacity 队列容量
+	 * @param action   定时执行的任务
+	 * @param take     每次取出元素个数
+	 * @param duration 执行间隔（毫秒）
+	 */
+	public ZCircularQueue(int capacity, Consumer<Collection<T>> action, int take, int duration) {
 		if (capacity > 0)
 			_capacity = capacity;
 		if (take > 0)
 			_take = take;
+		if (duration > 0)
+			_duration = duration;
+
 		_action = action;
 		_queue = new ArrayBlockingQueue<>(_capacity, true);
 		//定时器，定时取元素
-		ZTimer.start(this::peek, 200);
+		ZTimer.start(this::peek, _duration);
 	}
 
 	/**
